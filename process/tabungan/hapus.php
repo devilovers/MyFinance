21d35e4330bb925762b92191e9e74f8ff8e7116a
Sabila <nrislmsbl@gmail.com>
2026-06-29 09:36:35 +0800

    Feat implement automatic transaction deletion when a savings target is removed

--- process/tabungan/hapus.php
+++ process/tabungan/hapus.php
@@ -1,13 +1,36 @@
 <?php
 include '../../config/koneksi.php';
 
-$id = $_GET['id'];
+if (isset($_GET['id'])) {
+    $id = mysqli_real_escape_string($conn, $_GET['id']);
 
-mysqli_query($conn, "
-    DELETE FROM tabungan
-    WHERE id = '$id'
-");
+    $queryCek = mysqli_query($conn, "SELECT nama_target FROM tabungan WHERE id = '$id'");
+    
+    if (mysqli_num_rows($queryCek) > 0) {
+        $data = mysqli_fetch_assoc($queryCek);
+        $nama_target = $data['nama_target'];
+        $deskripsi_transaksi = "Alokasi Dana Tabungan: " . $nama_target;
 
-header('Location: ../../pages/tabungan.php');
-exit;
+        mysqli_begin_transaction($conn);
+
+        try {
+            mysqli_query($conn, "DELETE FROM tabungan WHERE id = '$id'");
+            mysqli_query($conn, "DELETE FROM transaksi WHERE deskripsi = '$deskripsi_transaksi'");
+
+            mysqli_commit($conn);
+            header("Location: ../../pages/tabungan.php?status=success");
+            exit;
+        } catch (Exception $e) {
+            mysqli_rollback($conn);
+            header("Location: ../../pages/tabungan.php?status=error");
+            exit;
+        }
+    } else {
+        header("Location: ../../pages/tabungan.php");
+        exit;
+    }
+} else {
+    header("Location: ../../pages/tabungan.php");
+    exit;
+}
 ?>
