542130374bf38a4dc037bd6ceac409af7321374c
Sabila <nrislmsbl@gmail.com>
2026-07-02 13:01:36 +0800

    Feat integrate automatic transaction logging when editing savings

--- process/tabungan/edit.php
+++ process/tabungan/edit.php
@@ -5,15 +5,42 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id = mysqli_real_escape_string($conn, $_POST['id']);
     $nama_target = mysqli_real_escape_string($conn, $_POST['nama_target']);
     $target = str_replace('.', '', $_POST['target']);
-    $terkumpul = str_replace('.', '', $_POST['terkumpul']);
+    $terkumpul_baru = str_replace('.', '', $_POST['terkumpul']);
     $tanggal = mysqli_real_escape_string($conn, $_POST['tanggal']);
+
+    $query_lama = mysqli_query($conn, "SELECT terkumpul FROM tabungan WHERE id = '$id'");
+    $data_lama = mysqli_fetch_assoc($query_lama);
+    $terkumpul_lama = $data_lama['terkumpul'] ?? 0;
+
+    $selisih = $terkumpul_baru - $terkumpul_lama;
+
+    if ($selisih != 0) {
+        if ($selisih > 0) {
+            $jenis = 'Pengeluaran';
+            $jumlah_transaksi = $selisih;
+            $deskripsi = "Penambahan tabungan untuk: " . $nama_target;
+        } else {
+            $jenis = 'Pemasukan';
+            $jumlah_transaksi = abs($selisih);
+            $deskripsi = "Penarikan/pengurangan tabungan dari: " . $nama_target;
+        }
+
+        $kategori = 'Tabungan'; 
+        $metode_pembayaran = 'Cash'; 
+        $tanggal_sekarang = date('Y-m-d');
+
+        mysqli_query($conn, "
+            INSERT INTO transaksi (jenis, kategori, metode_pembayaran, jumlah, tanggal, deskripsi)
+            VALUES ('$jenis', '$kategori', '$metode_pembayaran', '$jumlah_transaksi', '$tanggal_sekarang', '$deskripsi')
+        ");
+    }
 
     mysqli_query($conn, "
         UPDATE tabungan
         SET
             nama_target = '$nama_target',
             target = '$target',
-            terkumpul = '$terkumpul',
+            terkumpul = '$terkumpul_baru',
             tanggal = '$tanggal',
             updated_at = NOW()
         WHERE id = '$id'
